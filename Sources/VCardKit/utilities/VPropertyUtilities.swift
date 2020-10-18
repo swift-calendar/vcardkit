94c23b01ffa73fe0a7e8250b7e238d58038cc1bd
fwcd <fwcdmail@gmail.com>
2020-10-18 19:43:37 +0200

    Merge parameter collections in a stable way

--- Sources/VCardKit/utilities/VPropertyUtilities.swift
+++ Sources/VCardKit/utilities/VPropertyUtilities.swift
@@ -1,5 +1,32 @@
+private struct Pair<I>: Comparable & Hashable where I: Comparable & Hashable {
+    let x: I
+    let y: I
+
+    static func <(lhs: Self, rhs: Self) -> Bool {
+        [lhs.x, lhs.y].lexicographicallyPrecedes([rhs.x, rhs.y])
+    }
+}
+
+private struct Indexed<I, T>: Comparable & Hashable where I: Comparable & Hashable, T: Hashable {
+    let index: I
+    let value: T
+
+    static func ==(lhs: Self, rhs: Self) -> Bool {
+        lhs.index == rhs.index
+    }
+
+    static func <(lhs: Self, rhs: Self) -> Bool {
+        lhs.index < rhs.index
+    }
+}
+
 func merge(parameterCollections: [[(String, [String])]]) -> [(String, [String])] {
-    Array(parameterCollections
-        .map { Dictionary(uniqueKeysWithValues: $0) }
-        .reduce([:]) { $0.merging($1, uniquingKeysWith: +) })
+    parameterCollections
+        .enumerated()
+        .map { (x, d) in Dictionary(uniqueKeysWithValues: d
+            .enumerated()
+            .map { (y, t) in (Indexed(index: Pair(x: x, y: y), value: t.0), t.1) }) }
+        .reduce([:]) { $0.merging($1, uniquingKeysWith: +) }
+        .sorted { $0.key < $1.key }
+        .map { (i, v) in (i.value, v) }
 }
